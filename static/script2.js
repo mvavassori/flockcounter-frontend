1d9567832c24689dd9a3689d1030d7996ca7f27f
Marco Vavassori <mv@marcovavassori.com>
2024-04-08 21:01:20 +0200

    progress

--- static/script2.js
+++ static/script2.js
@@ -1,7 +1,7 @@
 // Prepare payload data
 const now = new Date();
 const formattedStamp = now.toISOString();
-const url = 'http://localhost:8080/api/visit';
+const url = "http://localhost:8080/api/visit";
 
 // Get the current time in milliseconds when the page loads
 let startTime = performance.now();
@@ -10,100 +10,137 @@ let currentUrl = window.location.href;
 
 let currentReferrer = document.referrer || null;
 
+console.log("window.location.host", window.location.host);
+// let isFirstVisit = true;
+let isFirstVisit =
+  !currentReferrer || !currentReferrer.includes(window.location.host);
+
 let previousPathname = window.location.pathname;
 
-console.log('Page loaded, startTime:', startTime);
+console.log("Page loaded, startTime:", startTime);
 
 // Function to handle sending the visit data
 function sendVisit(elapsedTime) {
-    const payloadData = {
-        timestamp: formattedStamp,
-        referrer: currentReferrer,
-        url: currentUrl,
-        // pathname: window.location.pathname,
-        pathname: previousPathname,
-        userAgent: navigator.userAgent,
-        language: navigator.language,
-        timeSpentOnPage: Math.round(elapsedTime)
-    };
-    let data = JSON.stringify(payloadData);
-    console.log('Sending visit data:', payloadData);
-    navigator.sendBeacon(url, data);
+  const payloadData = {
+    timestamp: formattedStamp,
+    referrer: currentReferrer,
+    url: currentUrl,
+    // pathname: window.location.pathname,
+    pathname: previousPathname,
+    userAgent: navigator.userAgent,
+    language: navigator.language,
+    timeSpentOnPage: Math.round(elapsedTime),
+    isUniqueVisit: isFirstVisit,
+  };
+  let data = JSON.stringify(payloadData);
+  console.log("Sending visit data:", payloadData);
+  navigator.sendBeacon(url, data);
 }
 
 // Event listener for page visibility changes
 window.addEventListener("visibilitychange", (event) => {
-    if (document.visibilityState === 'visible') {
-        // Page became visible, restart the timer
-        startTime = performance.now();
-        console.log('Page became visible, startTime updated:', startTime);
+  if (document.visibilityState === "visible") {
+    // Page became visible, restart the timer
+    startTime = performance.now();
+    console.log("Page became visible, startTime updated:", startTime);
+  } else {
+    // Page became hidden
+    const elapsedTime = performance.now() - startTime;
+    console.log("Page became hidden, elapsed time:", elapsedTime);
+    totalElapsedTime += elapsedTime;
+    console.log("Total elapsed time:", totalElapsedTime);
+    if (totalElapsedTime < 5000) {
+      console.log("Visit time less than 5 seconds, not sending data.");
+      // Reset the timer without sending the visit data
+      startTime = 0;
+      totalElapsedTime = 0;
+      console.log(
+        "Timer reset, startTime:",
+        startTime,
+        "totalElapsedTime:",
+        totalElapsedTime
+      );
     } else {
-        // Page became hidden
-        const elapsedTime = performance.now() - startTime;
-        console.log('Page became hidden, elapsed time:', elapsedTime);
-        totalElapsedTime += elapsedTime;
-        console.log('Total elapsed time:', totalElapsedTime);
-        if (totalElapsedTime < 5000) {
-            console.log('Visit time less than 5 seconds, not sending data.');
-            // Reset the timer without sending the visit data
-            startTime = 0;
-            totalElapsedTime = 0;
-            console.log('Timer reset, startTime:', startTime, 'totalElapsedTime:', totalElapsedTime);
-        } else {
-            sendVisit(totalElapsedTime);
-            // Reset the timer after sending the visit data
-            startTime = 0;
-            totalElapsedTime = 0;
-            console.log('Timer reset, startTime:', startTime, 'totalElapsedTime:', totalElapsedTime);
-        }
+      sendVisit(totalElapsedTime);
+      // Reset the timer after sending the visit data
+      startTime = 0;
+      totalElapsedTime = 0;
+      console.log(
+        "Timer reset, startTime:",
+        startTime,
+        "totalElapsedTime:",
+        totalElapsedTime
+      );
     }
+  }
 });
 
 // Event listener for route changes
-window.addEventListener('popstate', handleRouteChange);
+window.addEventListener("popstate", handleRouteChange);
 window.history.pushState = overridePushStateFunction(window.history.pushState);
-window.history.replaceState = overrideReplaceStateFunction(window.history.replaceState);
+window.history.replaceState = overrideReplaceStateFunction(
+  window.history.replaceState
+);
 
 function handleRouteChange() {
-    const newUrl = window.location.href;
-    if (newUrl !== currentUrl) {
-        // Store the current URL as the previous referrer
-        currentReferrer = currentUrl;
-        console.log('URL changed from', currentUrl, 'to', newUrl);
-        const elapsedTime = performance.now() - startTime;
-        console.log('Page changed, elapsed time:', elapsedTime);
-        totalElapsedTime += elapsedTime;
-        console.log('Total elapsed time:', totalElapsedTime);
-        if (totalElapsedTime < 5000) {
-            console.log('Visit time less than 5 seconds, not sending data.');
-            // Reset the timer without sending the visit data
-            startTime = performance.now();
-            totalElapsedTime = 0;
-            console.log('New page, startTime updated:', startTime, 'totalElapsedTime:', totalElapsedTime);
-        } else {
-            sendVisit(totalElapsedTime);
-            currentUrl = newUrl;
-            startTime = performance.now();
-            totalElapsedTime = 0;
-            console.log('New page, startTime updated:', startTime, 'totalElapsedTime:', totalElapsedTime);
-        }
+  const newUrl = window.location.href;
+  if (newUrl !== currentUrl) {
+    // Store the current URL as the previous referrer
+    currentReferrer = currentUrl;
+
+    // Set isFirstVisit to false after the first visit
+    // isFirstVisit = false;
+
+    // Set isFirstVisit to false after the first visit, but only if the referrer is from the same website
+    if (currentReferrer.includes(window.location.host)) {
+      isFirstVisit = false;
     }
+
+    console.log("URL changed from", currentUrl, "to", newUrl);
+    const elapsedTime = performance.now() - startTime;
+    console.log("Page changed, elapsed time:", elapsedTime);
+    totalElapsedTime += elapsedTime;
+    console.log("Total elapsed time:", totalElapsedTime);
+    if (totalElapsedTime < 5000) {
+      console.log("Visit time less than 5 seconds, not sending data.");
+      // Reset the timer without sending the visit data
+      startTime = performance.now();
+      totalElapsedTime = 0;
+      console.log(
+        "New page, startTime updated:",
+        startTime,
+        "totalElapsedTime:",
+        totalElapsedTime
+      );
+    } else {
+      sendVisit(totalElapsedTime);
+      currentUrl = newUrl;
+      startTime = performance.now();
+      totalElapsedTime = 0;
+      console.log(
+        "New page, startTime updated:",
+        startTime,
+        "totalElapsedTime:",
+        totalElapsedTime
+      );
+    }
+  }
 }
 
 function overridePushStateFunction(originalPushState) {
-    return function overridenPushState(...args) {
-        previousPathname = window.location.pathname; // Store the previous pathname before calling handleRouteChange
-        const result = originalPushState.apply(this, args);
-        handleRouteChange();
-        return result;
-    };
+  return function overridenPushState(...args) {
+    previousPathname = window.location.pathname; // Store the previous pathname before calling handleRouteChange
+    const result = originalPushState.apply(this, args);
+    handleRouteChange();
+    return result;
+  };
 }
 
 function overrideReplaceStateFunction(originalReplaceState) {
-    return function overridenReplaceState(...args) {
-        previousPathname = window.location.pathname; // Store the previous pathname before calling handleRouteChange
-        const result = originalReplaceState.apply(this, args);
-        handleRouteChange();
-        return result;
-    };
+  return function overridenReplaceState(...args) {
+    previousPathname = window.location.pathname;
+    const result = originalReplaceState.apply(this, args);
+    handleRouteChange();
+    return result;
+  };
 }
