b926b9d9ba495322e9347851765379959b3ba634
Marco Vavassori <mv@marcovavassori.com>
2024-09-03 20:42:11 +0200

    get sub expiry date

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -23,9 +23,7 @@ export default async function Profile() {
       </>
     );
   }
-  const user = await response.json();
-
-  console.log(user);
+  const data = await response.json();
 
   return (
     <div className="px-4 pb-4 pt-12 w-full">
@@ -33,39 +31,57 @@ export default async function Profile() {
       <div className="space-y-4">
         <div>
           <span className="block text-sm font-medium">Name:</span>
-          <span className="block text-sm">{user.name}</span>
+          <span className="block text-sm">{data.user.name}</span>
         </div>
         <div>
           <span className="block text-sm font-medium">Email:</span>
-          <span className="block text-sm">{user.email}</span>
+          <span className="block text-sm">{data.user.email}</span>
         </div>
         <div>
           <span className="block text-sm font-medium">Role:</span>
           <span className="block text-sm">
-            {user.role.charAt(0).toUpperCase() +
-              user.role.slice(1).toLowerCase()}
+            {data.user.role.charAt(0).toUpperCase() +
+              data.user.role.slice(1).toLowerCase()}
           </span>
         </div>
         <div>
           <span className="block text-sm font-medium">
             Subscription Status:
           </span>
-          <span className="block text-sm">
-            {user.subscription_status.charAt(0).toUpperCase() +
-              user.subscription_status.slice(1).toLowerCase()}
+          <span className="text-sm">
+            {data.user.subscription_status.charAt(0).toUpperCase() +
+              data.user.subscription_status.slice(1).toLowerCase()}{" "}
+          </span>
+          <span className="text-xs text-gray-700">
+            (Renews on:{" "}
+            {new Date(data.subscriptionExpiryDate).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+            )
           </span>
         </div>
         <div>
           <span className="block text-sm font-medium">Subscription Plan:</span>
-          {user.subscription_plan.String === "" ? (
+          {data.user.subscription_plan.String === "" ? (
             <span className="block text-sm">No plan available</span>
           ) : (
             <span className="block text-sm">
-              {user.subscription_plan.String.charAt(0).toUpperCase() +
-                user.subscription_plan.String.slice(1).toLowerCase()}
+              {data.user.subscription_plan.String.charAt(0).toUpperCase() +
+                data.user.subscription_plan.String.slice(1).toLowerCase()}
             </span>
           )}
-          {/* <span className="block text-sm">{user.subscription_plan.String}</span> */}
+        </div>
+        <div>
+          {/* <button
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={() => {
+              window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/stripe/customer-portal?session_id=${session?.user.id}`;
+            }}
+          >
+            Manage Subscription
+          </button> */}
         </div>
         <SignOutButton />
       </div>
