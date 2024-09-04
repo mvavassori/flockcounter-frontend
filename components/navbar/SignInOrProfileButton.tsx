df3a3a28d0f69143b10097eb6e2fabf0e00cd304
Marco Vavassori <mv@marcovavassori.com>
2024-09-04 14:50:28 +0200

    fix sign in not showing on first visit

--- components/navbar/SignInOrProfileButton.tsx
+++ components/navbar/SignInOrProfileButton.tsx
@@ -1,50 +1,11 @@
 "use client";
-
-import { useCallback, useEffect, useState } from "react";
-import { getSession, useSession } from "next-auth/react";
+//! WHY DID I MAKE THIS FUCKING CLIENT THING IN THE FIRST PLACE?!
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import UserIcon from "../icons/UserIcon";
-import { Session } from "next-auth";
-import { usePathname } from "next/navigation";
 
 export default function SignInOrProfileButton() {
-  // const { data: session } = useSession();
-  // console.log("buttonhandler session", session);
-
-  const [session, setSession] = useState<Session | null>(null);
-  // Changed the default status to loading
-  const [status, setStatus] = useState<string>("loading");
-
-  const pathName = usePathname();
-
-  const retrieveSession = useCallback(async () => {
-    try {
-      const sessionData = await getSession();
-      if (sessionData) {
-        setSession(sessionData);
-        setStatus("authenticated");
-        return;
-      }
-
-      setStatus("unauthenticated");
-    } catch (error) {
-      setStatus("unauthenticated");
-      setSession(null);
-    }
-  }, []);
-
-  useEffect(() => {
-    // We only want to retrieve the session when there is no session
-    if (!session) {
-      retrieveSession();
-    }
-
-    // use the pathname to force a re-render when the user navigates to a new page
-  }, [retrieveSession, session, pathName]);
-
-  // if (status === "loading") {
-  //   return null; // Renders nothing while loading
-  // }
+  const { status } = useSession();
   return (
     <>
       {status === "authenticated" ? (
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-import SignInOrProfileButton from "./SignInOrProfileButton";
+// import SignInOrProfileButton from "./SignInOrProfileButton";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import UserIcon from "../icons/UserIcon";
 
 const Navbar = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <nav className="fixed top-0 left-0 right-0 flex items-center justify-between lg:px-6 p-4 z-50 bg-white border-b-2 border-black">
       <div className="block flex-none md:hidden">
@@ -39,7 +43,19 @@ const Navbar = async () => {
         </ul>
       </div>
       <div className="flex justify-end md:w-1/3">
-        <SignInOrProfileButton />
+        {/* <SignInOrProfileButton /> */}
+        {session !== null ? (
+          <Link href="/profile" className="mr-6 hidden md:block">
+            <UserIcon />
+          </Link>
+        ) : (
+          <Link
+            href="/api/auth/signin"
+            className="hidden md:block mr-4 text-sm hover:bg-blue-600 justify-end bg-blue-500 text-white px-2 py-1 rounded-full"
+          >
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
