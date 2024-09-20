b97d63e2dc7a0fbd6e51991b1b81ccaab1727bab
Marco Vavassori <mv@marcovavassori.com>
2024-09-20 12:18:02 +0200

    remove unused deps

--- components/CustomTooltip.tsx
+++ components/CustomTooltip.tsx
@@ -1,105 +1,5 @@
 import { TooltipProps } from "recharts";
-
-function formatDate(
-  dateString: string,
-  period: string,
-  interval: string
-): string {
-  const date = parseDate(dateString, interval);
-
-  if (!date) {
-    throw new Error("Invalid date string or format");
-  }
-
-  switch (interval) {
-    case "hour":
-      if (period === "today" || period === "yesterday") {
-        return date.toLocaleString("en-US", {
-          weekday: "short",
-          day: "numeric",
-          month: "short",
-          hour: "numeric",
-          hour12: true,
-        });
-      }
-      break;
-    case "day":
-      if (["week", "month", "month-to-date", "last-month"].includes(period)) {
-        return date.toLocaleString("en-US", {
-          weekday: "short",
-          day: "numeric",
-          month: "short",
-        });
-      }
-      break;
-    case "month":
-      if (["year-to-date", "last-12-months", "last-5-years"].includes(period)) {
-        return date.toLocaleString("en-US", {
-          month: "short",
-          year: "numeric",
-        });
-      }
-      break;
-  }
-
-  throw new Error("Invalid combination of period and interval");
-}
-
-function parseDate(dateString: string, interval: string): Date | null {
-  const currentYear = new Date().getFullYear();
-
-  if (interval === "hour") {
-    const hour = parseInt(dateString, 10);
-    if (isNaN(hour) || hour < 0 || hour > 23) return null;
-    const date = new Date();
-    date.setHours(hour, 0, 0, 0);
-    return date;
-  } else if (interval === "day") {
-    const [day, month] = dateString.split(" ");
-    const monthIndex = getMonthIndex(month);
-    if (monthIndex === -1 || isNaN(parseInt(day))) return null;
-    return new Date(currentYear, monthIndex, parseInt(day));
-  } else if (interval === "month") {
-    const [month, year] = dateString.split(" ");
-    const monthIndex = getMonthIndex(month);
-    if (monthIndex === -1 || isNaN(parseInt(year))) return null;
-    return new Date(parseInt(year), monthIndex, 1);
-  }
-  return null;
-}
-
-function getMonthIndex(month: string): number {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  return months.findIndex((m) => m.toLowerCase() === month.toLowerCase());
-}
-
-// Helper function to format duration in seconds to hh:mm:ss or mm:ss
-const formatDuration = (totalSeconds: number) => {
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = totalSeconds % 60;
-
-  if (hours > 0) {
-    return `${hours}h ${minutes}m ${seconds}s`;
-  } else if (minutes > 0) {
-    return `${minutes}m ${seconds}s`;
-  } else {
-    return `${seconds}s`;
-  }
-};
+import { formatTooltipDate, formatDuration } from "@/utils/helper";
 
 export default function CustomTooltip({
   active,
@@ -115,7 +15,7 @@ export default function CustomTooltip({
 }): JSX.Element | null {
   if (active && payload && payload.length) {
     // Format the date with the day of the week
-    const formattedDate = formatDate(label, period, interval);
+    const formattedDate = formatTooltipDate(label, period, interval);
 
     // Get the current value from the payload
     const value = payload[0]?.value;
