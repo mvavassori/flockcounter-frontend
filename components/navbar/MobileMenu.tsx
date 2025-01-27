92758f07586e49096d4df8ddacc0d21162b9085d
Marco Vavassori <mv@marcovavassori.com>
2025-01-27 19:11:52 +0100

    navbar adjustments

--- components/navbar/MobileMenu.tsx
+++ components/navbar/MobileMenu.tsx
@@ -8,15 +8,22 @@ import { useSession } from "next-auth/react";
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMobileMenu = () => setIsOpen(!isOpen);
-
-  // Close the mobile menu when the overlay is clicked
   const closeMobileMenu = () => setIsOpen(false);
-
   const { status } = useSession();
+
+  const commonLinks = [
+    { href: "/", label: "Home" },
+    { href: "/pricing", label: "Pricing" },
+    { href: "/about", label: "About" },
+  ];
+
+  const authenticatedLinks = [
+    { href: "/websites", label: "Websites" },
+    { href: "/profile", label: "Profile" },
+  ];
 
   return (
     <div>
-      {/* Overlay */}
       {isOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-10"
@@ -29,58 +36,40 @@ export default function MobileMenu() {
             isOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out`}
         >
-          {/* Menu Items */}
           <div className="mt-12 flex flex-col items-start p-4">
-            {status === "authenticated" ? (
+            {commonLinks.map((link) => (
               <Link
-                href="/profile"
+                key={link.href}
+                href={link.href}
                 className="text-lg block p-2 w-full"
                 onClick={toggleMobileMenu}
               >
-                Profile
+                {link.label}
               </Link>
+            ))}
+
+            {status === "authenticated" ? (
+              authenticatedLinks.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className="text-lg block p-2 w-full"
+                  onClick={toggleMobileMenu}
+                >
+                  {link.label}
+                </Link>
+              ))
             ) : (
               <Link
-                href="/signin"
+                href="/api/auth/signin"
                 className="text-lg block p-2 w-full"
                 onClick={toggleMobileMenu}
               >
                 Sign In
               </Link>
             )}
-
-            <Link
-              href="/"
-              className="text-lg block p-2 w-full"
-              onClick={toggleMobileMenu}
-            >
-              Home
-            </Link>
-            <Link
-              href="/websites"
-              className="text-lg block p-2 w-full"
-              onClick={toggleMobileMenu}
-            >
-              Websites
-            </Link>
-            <Link
-              href="/pricing"
-              className="text-lg block p-2 w-full"
-              onClick={toggleMobileMenu}
-            >
-              Pricing
-            </Link>
-            <Link
-              href="/about"
-              className="text-lg block p-2 w-full"
-              onClick={toggleMobileMenu}
-            >
-              About
-            </Link>
-            {/* Add other links similarly */}
           </div>
         </div>
-        {/* Toggle Button */}
         <button onClick={toggleMobileMenu} className="z-30">
           {isOpen ? <CloseIcon width={24} height={24} /> : <HamburgerIcon />}
         </button>
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,21 +1,62 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-// import SignInOrProfileButton from "./SignInOrProfileButton";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import UserIcon from "../icons/UserIcon";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
-  return (
+
+  const UnauthenticatedNavbar = (
     <nav className="fixed top-0 left-0 right-0 flex items-center justify-between lg:px-6 p-4 z-50 bg-white border-b-2 border-black">
       <div className="block flex-none md:hidden">
         <MobileMenu />
       </div>
       <div className="flex w-full items-center justify-center md:justify-normal">
         <Link href="/" className="text-xl font-bold">
-          <span className="text-slate-500">flock</span>
-          <span>counter</span>
+          <span className="text-slate-500">Flock</span>
+          <span>Counter</span>
+        </Link>
+
+        <ul className="ml-12 hidden gap-6 text-sm md:flex md:items-center">
+          <li>
+            <Link
+              href="/pricing"
+              className="underline-offset-4 hover:underline"
+            >
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://github.com/mvavassori/flockcounter"
+              className="underline-offset-4 hover:underline"
+            >
+              GitHub
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-end md:w-1/3">
+        <Link
+          href="/profile"
+          className="mr-6 hidden md:block text-sm hover:bg-blue-600 justify-end bg-blue-500 text-white px-2 py-1 rounded-full"
+        >
+          Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+
+  const AuthenticatedNavbar = (
+    <nav className="fixed top-0 left-0 right-0 flex items-center justify-between lg:px-6 p-4 z-50 bg-white border-b-2 border-black">
+      <div className="block flex-none md:hidden">
+        <MobileMenu />
+      </div>
+      <div className="flex w-full items-center justify-center md:justify-normal">
+        <Link href="/" className="text-xl font-bold">
+          <span className="text-slate-500">Flock</span>
+          <span>Counter</span>
         </Link>
 
         <ul className="ml-12 hidden gap-6 text-sm md:flex md:items-center">
@@ -32,33 +73,26 @@ const Navbar = async () => {
               href="/pricing"
               className="underline-offset-4 hover:underline"
             >
-              Pricing
+              Plans
             </Link>
           </li>
-          <li>
-            <Link href="/about" className="underline-offset-4 hover:underline">
-              About
-            </Link>
-          </li>
+          <Link
+            href="https://github.com/mvavassori/flockcounter"
+            className="underline-offset-4 hover:underline"
+          >
+            GitHub
+          </Link>
         </ul>
       </div>
       <div className="flex justify-end md:w-1/3">
-        {/* <SignInOrProfileButton /> */}
-        {session !== null ? (
-          <Link href="/profile" className="mr-6 hidden md:block">
-            <UserIcon />
-          </Link>
-        ) : (
-          <Link
-            href="/api/auth/signin"
-            className="hidden md:block mr-4 text-sm hover:bg-blue-600 justify-end bg-blue-500 text-white px-2 py-1 rounded-full"
-          >
-            Sign In
-          </Link>
-        )}
+        <Link href="/profile" className="mr-6 hidden md:block">
+          <UserIcon />
+        </Link>
       </div>
     </nav>
   );
+
+  return session ? AuthenticatedNavbar : UnauthenticatedNavbar;
 };
 
 export default Navbar;
